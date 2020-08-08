41d3d005635a155179aca35a69c8cd3a34bda67c
jan <janpoonthong628@gmail.com>
2020-08-08 10:07:11 +0700

    Create hello app

---
+++ lecture3/hello/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def jan(request):
+    return HttpResponse("Hello, Jan!")
+
+def strager(request):
+    return HttpResponse("Hello, Strager!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+
+        })
