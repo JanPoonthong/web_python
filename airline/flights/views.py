234cd2e76de166c9864eafdafbcdbc06dca23c55
jan <janpoonthong628@gmail.com>
2020-08-27 12:53:04 +0700

    Delete Whitespaces

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -4,13 +4,11 @@ from django.urls import reverse
 
 from .models import Flight, Passenger
 
-
 # Create your views here.
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
-
 
 def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
@@ -19,7 +17,6 @@ def flight(request, flight_id):
         "passengers": flight.passengers.all(),
         "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
-
 
 def book(request, flight_id):
     if request.method == "POST":
