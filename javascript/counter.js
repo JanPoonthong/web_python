87c2e832ca30b4cdcb86a71bed2a674289cb5b7b
jan <janpoonthong628@gmail.com>
2020-08-28 09:46:32 +0700

    Move javascript code to counter.js

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,16 +1,16 @@
-if (!localStorage.getItem('counter')) {
-    localStorage.setItem('counter', 0);
-}
+let counter = 0;
 
 function count() {
-    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
-    localStorage.setItem('counter', counter);
+
+    if (counter % 10 == 0) {
+        alert(`Count is now ${counter}`);
+    }
 }
 
+// Loading content, because the document.quertSelector('button').onclick = count;
+// Is loaded first than <button>Count</button>
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
-
 });
