10ec6d685699c6e0e0cb232989e8692d05cc0bda
jan <janpoonthong628@gmail.com>
2020-08-31 19:56:37 +0700

    Create task.js
    Logic behide the task

---
+++ javascript/task/task.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // By default, submit button is disabled
+    document.querySelector('#submit').disabled = true;
+
+    document.querySelector('#task').onkeyup = () => {
+        if (document.querySelector('#task').value.length > 0) {
+            document.querySelector('#submit').disabled = false;
+        } else {
+            document.querySelector('#submit').disabled = true;
+        }
+    }
+
+    const task = document.querySelector('#task').value;
+
+    const li = document.createElement('li');
+    li.innerHTML = task;
+
+    document.querySelector('#tasks').append(li);
+
+    document.querySelector('#task').value = '';
+
+    document.querySelector('#submit').disabled = true;
+
+    // Stop from submitting
+    return false;
+});
+
