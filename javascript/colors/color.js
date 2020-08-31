9a9e2bfb1494770bc6cde8a1f4fc3f7e15bdcaa2
jan <janpoonthong628@gmail.com>
2020-08-31 10:49:57 +0700

    Change color base on select option
    Select ('select') and give a function and change to hello text base on
    user select option in html, .style.color = this.value
    
    this.value is get the value from html

--- javascript/colors/color.js
+++ javascript/colors/color.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 
-    document.querySelectorAll('button').forEach(function(button) {
-        button.onclick = function() {
-            document.querySelector('#hello').style.color = button.dataset.color;
-        }
-    });
+    document.querySelector('select').onchange = function() {
+        document.querySelector('#hello').style.color = this.value;
+    }
 });
