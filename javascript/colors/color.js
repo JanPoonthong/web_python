af76d89349b3cca4dae3d04902c3ebcfc8bcdaae
jan <janpoonthong628@gmail.com>
2020-08-31 09:40:13 +0700

    Add javascript to change the color of hello
    Change the color with class, which is mention is html

--- javascript/colors/color.js
+++ javascript/colors/color.js
@@ -1,2 +1,17 @@
-const friends = [];
-friends = null;
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Change font color to red
+    document.querySelector('#red').onclick = function() {
+        document.querySelector('#hello').style.color = 'red';
+    }
+
+    // Change font color to green
+    document.querySelector('#green').onclick = function() {
+        document.querySelector('#hello').style.color = 'green';
+    }
+
+    // Change font color to blue
+    document.querySelector('#blue').onclick = function() {
+        document.querySelector('#hello').style.color = 'blue';
+    }
+});
