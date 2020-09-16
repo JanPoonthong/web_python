800e4db3183c90fa84647e3b6bbd24f4ddcd8370
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-16 11:33:53 +0700

    setInterval(count, 1000);

--- javascript/count_num/counter.js
+++ javascript/count_num/counter.js
@@ -4,13 +4,17 @@ function count() {
     counter++;
     document.querySelector('h1').innerHTML = counter;
 
+    /*
     if (counter % 10 == 0) {
         alert(`Count is now ${counter}`);
     }
+    */
 }
 
 // Loading content, because the document.quertSelector('button').onclick = count;
 // Is loaded first than <button>Count</button>
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 1000);
 });
