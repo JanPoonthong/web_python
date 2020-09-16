b76a29e0ba8dd49e08cac3f9c893422f4b7fed16
Jan Poonthong <janpoonthong628@gmail.com>
2020-09-16 11:54:46 +0700

    Create localStrage

--- javascript/count_num/counter.js
+++ javascript/count_num/counter.js
@@ -1,8 +1,12 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
 function count() {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
     /*
     if (counter % 10 == 0) {
@@ -14,7 +18,8 @@ function count() {
 // Loading content, because the document.quertSelector('button').onclick = count;
 // Is loaded first than <button>Count</button>
 document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
 
-    setInterval(count, 1000);
+    // setInterval(count, 1000);
 });
