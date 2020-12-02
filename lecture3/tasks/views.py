64e837fc99ba014b23e401617ed639d1d0cff02c
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-12-02 09:41:29 +0700

    Add newyear to urls.py

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -8,11 +8,9 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Tasks")
 
 
-# Create your views here.
 def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
-
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
     })
